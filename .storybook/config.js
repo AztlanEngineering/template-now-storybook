18fe6126757f6762dc339c21cf334f8dc170e7df
Adrian RDP <rdp@779.mx>
2019-12-24 03:21:50 +0000

    Readme beta

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,4 +3,4 @@ import { configure } from '@storybook/react';
 //import 'scss/main.scss'
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(require.context('../src/stories', true, /\.stories\.(js|mdx)$/), module);
